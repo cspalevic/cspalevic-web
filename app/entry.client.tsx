4798554f7fad6523e3d77aee0cd160e8f05bed96
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2022-07-24 20:54:13 -0500

    Upgrade to react 18

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,14 +1,14 @@
-import { hydrate } from "react-dom";
-import { useEffect } from "react";
+import { hydrateRoot } from "react-dom/client";
+import { startTransition, useEffect } from "react";
 import { RemixBrowser, useMatches, useLocation } from "@remix-run/react";
-import * as Sentry from "@sentry/remix";
+import { init, remixRouterInstrumentation } from "@sentry/remix";
 import { BrowserTracing } from "@sentry/tracing";
 
-Sentry.init({
+init({
   dsn: window.ENV?.SENTRY_DSN,
   integrations: [
     new BrowserTracing({
-      routingInstrumentation: Sentry.remixRouterInstrumentation(
+      routingInstrumentation: remixRouterInstrumentation(
         useEffect,
         useLocation,
         useMatches
@@ -18,4 +18,14 @@ Sentry.init({
   tracesSampleRate: window.ENV?.NODE_ENV === "development" ? 1.0 : 0.2,
 });
 
-hydrate(<RemixBrowser />, document);
+const hydrate = () => {
+  startTransition(() => {
+    hydrateRoot(document, <RemixBrowser />);
+  });
+};
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  window.setTimeout(hydrate, 1);
+}
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,13 +1,19 @@
-import { renderToString } from "react-dom/server";
+import { PassThrough } from "stream";
+import { renderToPipeableStream } from "react-dom/server";
 import { RemixServer } from "@remix-run/react";
-import * as Sentry from "@sentry/remix";
+import { Response } from "@remix-run/node";
+import { init } from "@sentry/remix";
+import isBot from "isbot";
+import logger from "./models/logger.server";
 
 import type { EntryContext } from "@remix-run/node";
 
-Sentry.init({
+init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.2,
 });
+
+const ABORT_DELAY = 5000;
 
 const handleRequest = (
   request: Request,
@@ -15,15 +21,36 @@ const handleRequest = (
   responseHeaders: Headers,
   remixContext: EntryContext
 ) => {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  const callbackName = isBot(request.headers.get("user-agent"))
+    ? "onAllReady"
+    : "onShellReady";
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        [callbackName]: () => {
+          const body = new PassThrough();
+          responseHeaders.set("Content-Type", "text/html");
+          resolve(
+            new Response(body, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+          pipe(body);
+        },
+        onShellError: (error) => {
+          reject(error);
+        },
+        onError: (error) => {
+          didError = true;
+          logger.error("Error rendering page", error);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
 };
 
